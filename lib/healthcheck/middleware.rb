74834cddd8ffdb4100e5f7f677892f11ff9c7742
Jake Bell <jake@theunraveler.com>
2015-07-22 11:34:34 -0500

    Adding application class for use outside of rack

--- lib/healthcheck/middleware.rb
+++ lib/healthcheck/middleware.rb
@@ -1,6 +1,14 @@
 require 'healthcheck'
 
 module Healthcheck
+  class Application
+    def call(_env)
+      NewRelic::Agent.ignore_transaction if defined? NewRelic
+      report = Healthcheck::Report.new
+      [report.ok? ? 200 : 500, { 'Content-Type' => 'application/json' }, [report.to_json]]
+    end
+  end
+
   class Middleware
     def initialize(app, path = :healthcheck)
       @app = app
@@ -9,9 +17,7 @@ module Healthcheck
 
     def call(env)
       if env['PATH_INFO'] == "/#{@path}"
-        NewRelic::Agent.ignore_transaction if defined? NewRelic
-        report = Healthcheck::Report.new
-        [report.ok? ? 200 : 500, { 'Content-Type' => 'application/json' }, [report.to_json]]
+        Application.new.call(env)
       else
         @app.call(env)
       end
