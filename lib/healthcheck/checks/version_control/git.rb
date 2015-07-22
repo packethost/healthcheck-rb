babe5b631a2800ee21bf8f2b2797ceaf08ac25bd
Jake Bell <jake@theunraveler.com>
2015-07-22 01:18:55 -0500

    Refactoring git check, moving to VersionControl namespace

---
+++ lib/healthcheck/checks/version_control/git.rb
@@ -0,0 +1,29 @@
+require 'healthcheck/checks/abstract_check'
+
+module Healthcheck
+  module Checks
+    module VersionControl
+      class Git < AbstractCheck
+        def initialize(sha = nil)
+          @sha = sha
+        end
+
+        def report
+          result || :unknown
+        end
+
+        private
+
+        def perform
+          @sha || find_sha_from_git
+        end
+
+        def find_sha_from_git
+          `git --git-dir="#{Rails.root.join('.git')}" rev-parse --short HEAD`.strip
+        rescue
+          nil
+        end
+      end
+    end
+  end
+end
