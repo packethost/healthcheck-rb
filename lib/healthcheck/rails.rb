b6618f0f785129cf25e1883cb272b8e106e7dd8e
Jake Bell <jake@theunraveler.com>
2015-07-27 10:29:04 -0500

    Better positioning of middleware

--- lib/healthcheck/rails.rb
+++ lib/healthcheck/rails.rb
@@ -5,7 +5,19 @@ module Healthcheck
   class Railtie < Rails::Railtie
     initializer 'healthcheck.configure_rails_initialization' do
       Healthcheck.configuration.logger = Rails.logger
-      app.middleware.insert_before 0, Healthcheck::Middleware
+
+      case
+      when defined?(Rack::Head)
+        app.middleware.insert_after Rack::Head, Healthcheck::Middleware
+      when defined?(ActiveRecord::ConnectionAdapters::ConnectionManagement)
+        app.middleware.insert_after ActiveRecord::ConnectionAdapters::ConnectionManagement, Healthcheck::Middleware
+      when defined?(ActionDispatch::Callbacks)
+        app.middleware.insert_after ActionDispatch::Callbacks, Healthcheck::Middleware
+      when defined?(Rails::Rack::Logger)
+        app.middleware.insert_after Rails::Rack::Logger, Healthcheck::Middleware
+      else
+        app.middleware.use Healthcheck::Middleware
+      end
     end
 
     private
